de5fce0cbe30b308b549273baedb4faddeb39fee
Cymen Vig <cymenvig@gmail.com>
2014-04-24 16:10:46 -0500

    Update examples so can run from any directory

--- examples/image_search.rb
+++ examples/image_search.rb
@@ -1,7 +1,7 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'getty_connect'
 
-GettyConnect::Configuration.load(File.join(Dir.pwd, '..', 'config.yml'))
+GettyConnect::Configuration.load(File.expand_path('../../config.yml', __FILE__))
 @token = GettyConnect.token
 query = {
   Query: {
--- examples/token.rb
+++ examples/token.rb
@@ -1,5 +1,5 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'getty_connect'
 
-GettyConnect::Configuration.load(File.join(Dir.pwd, '..', 'config.yml'))
+GettyConnect::Configuration.load(File.expand_path('../../config.yml', __FILE__))
 puts GettyConnect.token
